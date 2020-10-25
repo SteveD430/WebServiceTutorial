b3f66c79a952297454bf9c1734c16cdb81e013fc
Steve Dickinson <steve.dickinson@btinternet.com>
2020-10-25 13:48:44 +0000

    Removed Weather Key

--- WebServiceTutorial/Constants.cs
+++ WebServiceTutorial/Constants.cs
@@ -4,6 +4,6 @@ namespace WebServiceTutorial
     public static class Constants
     {
         public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-        public const string OpenWeathermapAPIKey = "648676a6d3e90936d2aab98a1d3f114b";
+        public const string OpenWeathermapAPIKey = "";
     }
 }
